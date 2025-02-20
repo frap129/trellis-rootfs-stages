d8d4605d8ade6a0ba84f51eedc12b9d31b91ffb3
Joe Maples <joe@maples.dev>
2025-02-20 14:27:58 -0500

    feat(finalize): Update for bootc usage

--- finalize/scripts/dracut-setup.sh
+++ finalize/scripts/dracut-setup.sh
@@ -4,10 +4,9 @@ set -xeo pipefail
 args=('--force')
 
 for line in $(find /usr/lib/modules -name pkgbase); do
-	read -r pkgbase < "${line}"
+	read -r pkgbase <"${line}"
 	kver="${line#'/usr/lib/modules/'}"
 	kver="${kver%'/pkgbase'}"
 
-	install -Dm0644 "/${line%'/pkgbase'}/vmlinuz" "/boot/vmlinuz-${pkgbase}"
-	dracut "${args[@]}" "/boot/initramfs-${pkgbase}.img" --kver "$kver"
+	dracut "${args[@]}" "/${line%'/pkgbase'}/initramfs.img" --kver "$kver"
 done
